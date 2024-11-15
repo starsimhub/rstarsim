4a5120f3fa1344a4e1e85ae43b6a6c732875ee64
Cliff Kerr <questionably@gmail.com>
2024-11-14 20:37:33 -0800

    working

--- R/starsim.R
+++ R/starsim.R
@@ -1,22 +1,5 @@
 # All publicly visible functions
 
-# Imports
-# sc <- import('sciris')
-# np <- import('numpy')
-# pd <- import('pandas')
-# plt <- reticulate::import('matplotlib.pyplot')
-
-# Shortcuts to important classes
-# Sim <- ss$Sim
-# MultiSim <- ss$MultiSim
-# People <- ss$People
-# Module <- ss$Module
-# Demographics <- ss$Demographics
-# Network <- ss$Network
-# Disease <- ss$Disease
-# Intervention <- ss$Intervention
-# Analyzer <- ss$Analyzer
-
 #' Install Starsim
 #'
 #' @return
@@ -31,7 +14,7 @@ install_starsim <- function(..., envname = "r-starsim") {
   reticulate::use_virtualenv("r-starsim", required = FALSE)
 }
 
-#' Load Starsim
+#' Load Starsim alone
 #'
 #' @return
 #' @export
@@ -41,18 +24,48 @@ load_starsim <- function() {
   reticulate::import('starsim')
 }
 
-#' @name ss
-#' @title Starsim module
-#' @description The complete Starsim module
-#' @export
-ss <- reticulate::import('starsim')
 
-#' #' Show any Python plots that have been generated
-#' #'
-#' #' @return
-#' #' @export
-#' #'
-#' #' @examples
-#' pyshow <- function() {
-#'     plt$show
-#' }
+#' Load all components of the Starsim environment
+#'
+#' @return
+#' @export
+#'
+#' @examples
+load_starsim_env <- function() {
+
+  # Imports
+  ss <- reticulate::import('starsim')
+  sc <- reticulate::import('sciris')
+  np <- reticulate::import('numpy')
+  pd <- reticulate::import('pandas')
+  plt <- reticulate::import('matplotlib.pyplot')
+
+  # Assign imports
+  assign("ss", ss, envir = .GlobalEnv)
+  assign("sc", sc, envir = .GlobalEnv)
+  assign("np", np, envir = .GlobalEnv)
+  assign("pd", pd, envir = .GlobalEnv)
+  assign("plt", plt, envir = .GlobalEnv)
+
+  # Shortcuts to important classes
+  Sim <- ss$Sim
+  MultiSim <- ss$MultiSim
+  Module <- ss$Module
+  Demographics <- ss$Demographics
+  Network <- ss$Network
+  Connector <- ss$Connector
+  Disease <- ss$Disease
+  Intervention <- ss$Intervention
+  Analyzer <- ss$Analyzer
+
+  # Assign classes
+  assign("Sim", Sim, envir = .GlobalEnv)
+  assign("MultiSim", MultiSim, envir = .GlobalEnv)
+  assign("Module", Module, envir = .GlobalEnv)
+  assign("Demographics", Demographics, envir = .GlobalEnv)
+  assign("Network", Network, envir = .GlobalEnv)
+  assign("Connector", Connector, envir = .GlobalEnv)
+  assign("Disease", Disease, envir = .GlobalEnv)
+  assign("Intervention", Intervention, envir = .GlobalEnv)
+  assign("Analyzer", Analyzer, envir = .GlobalEnv)
+}
--- examples/examples.R
+++ examples/examples.R
@@ -1,22 +1,25 @@
 # Load Starsim
-library(starsim)
+library(rstarsim)
+load_starsim_env()
+
+ss$options(jupyter=TRUE)
 
 # Run a simple demo
 s1 <- ss$demo()
 
 # Full example
 pars <- list(
-    n_agents = n_agents,
+    n_agents = 10000,
     birth_rate = 20,
     death_rate = 15,
     networks = list(
         type = 'randomnet',
-        n_contacts = 4,
+        n_contacts = 4
     ),
     diseases = list(
         type = 'sir',
         dur_inf = 10,
-        beta = 0.1,
+        beta = 0.1
     )
 )
 
